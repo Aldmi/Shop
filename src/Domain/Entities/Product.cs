ac5c4b3b9934f4efaa0cd59cd2de88ee10baba06
Aldmi <Aldmi@bk.ru>
2016-05-21 22:35:37 +0600

    Fix Validation of the fractional part in decimal type

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -6,8 +6,11 @@ namespace Domain.Entities
 	{
 		[Key]
 		public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите название продукта")]
 		public string Name { get; set; }
-		public decimal UnitPrice { get; set; } 
+
+        public decimal UnitPrice { get; set; } 
 
         public string PictureRef { get; set; }
 	}
---
+++ src/WebUI/Migrations/201605191311300_AddNameRequired.cs
@@ -0,0 +1,18 @@
+namespace WebUI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameRequired : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Products", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "Name", c => c.String());
+        }
+    }
+}
